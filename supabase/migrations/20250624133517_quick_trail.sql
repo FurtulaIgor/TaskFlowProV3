f5fe75fdee19d02ab65fd415b199ad9c079124cb
FurtulaIgor <58146193+FurtulaIgor@users.noreply.github.com>
2025-06-29 17:39:58 +0200

    Updated 20250624133517_quick_trail.sql

--- supabase/migrations/20250624133517_quick_trail.sql
+++ supabase/migrations/20250624133517_quick_trail.sql
@@ -74,8 +74,3 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Create trigger to automatically update updated_at
-CREATE TRIGGER update_user_profiles_updated_at
-  BEFORE UPDATE ON user_profiles
-  FOR EACH ROW
-  EXECUTE FUNCTION update_updated_at_column();
